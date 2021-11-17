f7717ad6980bebe87ff2796c4dafd814e55cc671
nnuyz <hellonedesign@gmail.com>
2021-11-17 20:20:51 +0300

    master

--- Wrapper/src/app.js
+++ Wrapper/src/app.js
@@ -21,6 +21,7 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
+
 const toggleTabs = (target) => {
   const tabs = Array.from(document.querySelectorAll('.who-tabs .tab'))
 
@@ -28,6 +29,15 @@ const toggleTabs = (target) => {
     e.classList.remove('active')
   })
   target.classList.add('active')
+
+  const tabsTxt = Array.from(document.querySelectorAll('.adv'))
+  tabsTxt.forEach(e => {
+    e.classList.remove('active')
+    if(e.dataset.id === target.dataset.id) {
+      e.classList.add('active')
+    }
+  })
+
 }
 
 const scrollTo = (target) => {
@@ -48,20 +58,94 @@ const windowClick = ($evt) => {
      target.closest('.qa-container').classList.toggle('active')
   } else if (target.closest('.anchor-link')) {
     scrollTo(target)
+  } else if (target.closest('.footer-button')) {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
   }
 }
 
-const footerBtn = document.querySelector('.footer-button')
+const initDatepickers = () => {
+  const datepickers = Array.from(document.querySelectorAll('.date-input'))
 
-function scrollPageUp() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
+  datepickers.forEach(e => {
+    const inputDataName = e.dataset.name
+
+    window.datepicker(`input[name=${inputDataName}]`, {
+      customDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пн', 'Сб', 'Вс'],
+      customMonths: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      formatter: (input, date, instance) => {
+        const value = date.toLocaleDateString()
+        input.value = value // => '1/1/2099'
+      }
+    })
   })
 }
 
-footerBtn.addEventListener('click', scrollPageUp)
-
-
-
 window.addEventListener('click', windowClick)
+initDatepickers()
+
+const citizenship = new window.Select({
+  placeholder: 'Выберите гражданство:',
+	dataset: [
+    {
+      value: 'Россия'
+    },
+    {
+      value: 'Иностранное'
+    },
+    {
+      value: 'Другое'
+    },
+    {
+      value: 'Лицо без гражданства'
+    }
+  ],
+	selected: '',
+}).componentMount({
+	el: document.querySelector('[ref="select-box-citizenship"]')
+})
+
+
+const documentType = new window.Select({
+  placeholder: 'Выберите документ:',
+	dataset: [
+    {
+      value: 'Паспорт'
+    },
+    {
+      value: 'Свидетельство о рождении,'
+    },
+    {
+      value: 'Другое'
+    }
+  ],
+	selected: '',
+}).componentMount({
+	el: document.querySelector('[ref="select-box-document"]')
+})
+
+const period = new window.Select({
+  placeholder: 'Выберите срок регистрации:',
+	dataset: [
+    {
+      value: '3 месяца'
+    },
+    {
+      value: '6 месяца'
+    },
+    {
+      value: '1 год'
+    },
+    {
+      value: '3 года'
+    },
+    {
+      value: '5 лет'
+    }
+  ],
+	selected: '',
+}).componentMount({
+	el: document.querySelector('[ref="select-box-period"]')
+})
